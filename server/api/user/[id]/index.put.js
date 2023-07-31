128c3eec2e2df5921f4b25794a772582f203edd2
Aras emr <arasemr1234@gmail.com>
2023-08-01 01:43:12 +0300

    profilePhoto upload fix!

--- server/api/user/[id]/index.put.js
+++ server/api/user/[id]/index.put.js
@@ -1,8 +1,10 @@
 import userModel from "../../../models/user";
-import {writeFileSync, rmSync} from "fs";
+import * as fs from "fs";
 import { getUser } from "../../../util/getUser";
 import {extname} from "path"
 import {upload} from "../../../util/hizliresim";
+import * as path from "path";
+import { fileURLToPath } from "url";
 
 export default defineEventHandler(async(event) => {
     const id = getRouterParams(event).id;
@@ -15,13 +17,20 @@ export default defineEventHandler(async(event) => {
     const files = await readMultipartFormData(event);
     const profilePhoto = files[0];
 
-    const filePath = `server/temp/${Math.floor(Math.random()*Date.now()).toString(36)}${extname(profilePhoto.filename)}`;
+    const __filename = fileURLToPath(import.meta.url);
 
-    writeFileSync(filePath,profilePhoto.data);
+    const __dirname = path.dirname(__filename);
+
+    let temp = path.join(__dirname,"temp");
+    if(!fs.existsSync(temp)) fs.mkdirSync(temp);
+    
+    const filePath = path.join(temp,`${Math.floor(Math.random()*Date.now()).toString(36)}${extname(profilePhoto.filename)}`);
+
+    fs.writeFileSync(filePath, profilePhoto.data);
 
     let {imageUrl} = await upload(filePath);
 
-    rmSync(filePath);
+    fs.rmSync(filePath);
 
     my.profilePhoto = imageUrl;
     await my.save();
