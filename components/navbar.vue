6ee8f01cd909344c7bda0349c15902a7d5237fd9
Aras emr <arasemr1234@gmail.com>
2023-08-10 14:14:39 +0300

    v1.1.1
    Change Log:
    
    The product hunt logo has been relocated.

--- components/navbar.vue
+++ components/navbar.vue
@@ -28,11 +28,11 @@
             <nuxt-link to="/financial">financial status</nuxt-link>
             <div class="flex flex-col lg:flex-row items-center gap-5" v-if="user">
                 <nuxt-link to="/dashboard/">dashboard</nuxt-link>
-                <nuxt-link to="/">{{user.username}}</nuxt-link>
                 <div class="relative h-full">
                     <img @click="dropdown" :src="user.profilePhoto" width="32" draggable="false" class="rounded-full cursor-pointer max-h-full h-full" alt="">
                     <span class="border-2 border-green-600 animate-ping text-green-600 bg-green-600 w-2 h-2 block rounded-full absolute right-0 bottom-0"></span>
                     <div :class="`right-0 z-50 absolute bg-gray-700 py-2 px-4 rounded-lg flex flex-col gap-2 duration-300 ${dropdownIsOpen ? 'opacity-100 visible dropdownopenanim' : 'opacity-0 invisible dropdowncloseanim'}`">
+                        <span>{{user.id}}</span>
                         <nuxt-link to="/profile">profile</nuxt-link>
                         <span class="cursor-pointer" @click="logout">logout</span>
                     </div>
