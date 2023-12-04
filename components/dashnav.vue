8c3db2744684426d3d38502b61fa79159ef1ff1a
Aras emr <arasemr1234@gmail.com>
2023-12-04 23:34:17 +0300

    Mobile dashboard navbar

--- components/dashnav.vue
+++ components/dashnav.vue
@@ -1,3 +1,9 @@
+<script setup>
+
+const nav = ref(false);
+
+</script>
+
 <template>
     <div class="h-full w-full lg:w-1/6 py-2 px-4 bg-gray-800 gap-3 hidden lg:flex flex-col items-center">
         <nuxt-link to="/">
@@ -8,5 +14,11 @@
             <nuxt-link class="dashnav_link" to="/dashboard/friends">Friends</nuxt-link>
             <nuxt-link class="dashnav_link" to="/dashboard/profile">Profile</nuxt-link>
         </div>
+    </div>
+    <i class="lg:hidden block fixed top-0 left-0 fa-solid fa-bars fa-2x ml-4 mt-4 z-50" @click="nav = !nav"></i>
+    <div v-if="nav" class="fixed top-0 left-0 bg-black/50 w-full h-full flex flex-col items-center justify-center gap-5 z-40">
+        <nuxt-link to="/dashboard">Dashboard</nuxt-link>
+        <nuxt-link to="/dashboard/friends">Friends</nuxt-link>
+        <nuxt-link to="/dashboard/profile">Profile</nuxt-link>
     </div>
 </template>
